bd95994b4cb323d865880c65d5e93670fdabf157
juanc5ibanez <juanc5ibanez@hotmail.com>
2016-09-15 09:29:19 +1000

    Feeder works now

--- Storage.py
+++ Storage.py
@@ -1,18 +1,22 @@
+import uuid
+
 from cassandra.cluster import Cluster
 import Term
 
 class Storage:
 
-    def _init_(self):
-        pass
+    def Initialize(self):
+        session = self.GetSession()
+        session.execute("CREATE TABLE IF NOT EXISTS wlm.terms2( terms_id uuid, content text,creation_Date timestamp, PRIMARY KEY(content,terms_id))")
+        session.execute("CREATE TABLE IF NOT EXISTS wlm.user_updates(term text, created_at text, favorite_count int, favorited boolean, filter_level text, id_str text, lang text,possibly_sensitive boolean, retweet_count int,source text, text text, timestamp_ms text, user_id_str text, user_lang text, user_screen_name text, user_location text,creation_date timestamp , PRIMARY KEY(term,id_str) )")
 
     def SaveTerm(self,term):
         session = self.GetSession()
-        session.execute("insert into terms (terms_id,content,creation_Date) VALUES(%s,%s,%s)", (term.Id,term.Content,term.CreationDate))
+        session.execute("insert into terms2 (terms_id,content,creation_Date) VALUES(%s,%s,%s)", (term.Id,term.Content,term.CreationDate))
 
     def GetTerms(self):
         session = self.GetSession()
-        results = session.execute("select * from wlm.terms")
+        results = session.execute("select * from wlm.terms2")
         returnValues = []
         for row in results:
             returnValues.append(Term.Term(row.content,row.terms_id,row.creation_date))
@@ -20,7 +24,15 @@ class Storage:
 
     def DeteTerm(self,term):
         session = self.GetSession()
-        session.execute("delete from wlm.terms where terms_id = %s",term.Id)
+        session.execute("delete from wlm.terms2 where terms_id = %s",(term.Id,))
+
+    def SaveUserUpdate(self,userUpdate):
+        session = self.GetSession()
+        session.execute("insert into wlm.user_updates(term,created_at,favorite_count,favorited, filter_level, id_str, lang,possibly_sensitive, retweet_count,source, text, timestamp_ms, user_id_str, user_lang, user_screen_name, user_location,creation_date)"
+                        " VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
+                        (userUpdate.term,userUpdate.created_at,userUpdate.favorite_count,userUpdate.favorited,userUpdate.filter_level,userUpdate.id_str,
+                         userUpdate.lang,userUpdate.possibly_sensitive,userUpdate.retweet_count,userUpdate.source,userUpdate.text,userUpdate.timestamp_ms,
+                         userUpdate.user_id_str,userUpdate.user_lang,userUpdate.user_screen_name,userUpdate.user_location,userUpdate.creation_date))
 
 
     def GetSession(self):
---
+++ UserUpdate.py
@@ -0,0 +1,21 @@
+import datetime
+class UserUpdate:
+
+    def __init__(self,term,created_at,favorite_count,favorited,filter_level,id_str,lang,possibly_sensitive,retweet_count,source,text,timestamp_ms,user_id_str,user_lang,user_screen_name,user_location,creation_date = datetime.datetime.now()):
+        self.term = term
+        self.favorite_count = favorite_count
+        self.favorited = favorited
+        self.filter_level = filter_level
+        self.id_str = id_str
+        self.lang = lang
+        self.retweet_count = retweet_count
+        self.possibly_sensitive = possibly_sensitive
+        self.source = source
+        self.text = text
+        self.timestamp_ms = timestamp_ms
+        self.user_id_str = user_id_str
+        self.user_lang = user_lang
+        self.user_screen_name = user_screen_name
+        self.user_location = user_location
+        self.created_at = created_at
+        self.creation_date = creation_date
